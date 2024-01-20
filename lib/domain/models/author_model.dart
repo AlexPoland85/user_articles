df20a2d64c9fbef26df3ed366e202aaf8a2a957b
Alex Pawlowski <xander.pawlowski@gmail.com>
2024-01-20 12:09:27 +0100

    Implement freezed generator to the article_model and author_model

--- lib/domain/models/author_model.dart
+++ lib/domain/models/author_model.dart
@@ -1,20 +1,17 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'author_model.freezed.dart';
 part 'author_model.g.dart';
 
-@JsonSerializable()
-class AuthorModel {
-  const AuthorModel({
-    required this.id,
-    required this.picture,
-    @JsonKey(name: 'first_name') required this.firstName,
-    @JsonKey(name: 'last_name') required this.lastName,
-  });
-
-  final int id;
-  final String picture;
-  final String firstName;
-  final String lastName;
+@freezed
+class AuthorModel with _$AuthorModel {
+  const AuthorModel._();
+  factory AuthorModel(
+    int id,
+    String picture,
+    @JsonKey(name: 'first_name') String firstName,
+    @JsonKey(name: 'last_name') String lastName,
+  ) = _AuthorModel;
 
   String get name {
     return '$firstName $lastName';
@@ -22,6 +19,4 @@ class AuthorModel {
 
   factory AuthorModel.fromJson(Map<String, dynamic> json) =>
       _$AuthorModelFromJson(json);
-
-  Map<String, dynamic> toJson() => _$AuthorModelToJson(this);
 }
